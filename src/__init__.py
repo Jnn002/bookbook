de5fc1afa9a6c1db9f096c27e539cac58ea12467
John <jyaxp1@miumg.edu.gt>
2025-01-10 22:55:08 -0600

    Established connection to postgreSQL

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,21 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from src.books.routes import book_router
+from src.db.main import init_db
+
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    print('starting app')
+    await init_db()
+    yield
+    print('stopping app')
+
 
 version = '0.1.0'
 
-app = FastAPI(version=version)
+app = FastAPI(version=version, lifespan=life_span)
 
 app.include_router(book_router, prefix=f'/api/{version}/books', tags=['books'])
--- src/db/main.py
+++ src/db/main.py
@@ -1,6 +1,14 @@
 from sqlalchemy.ext.asyncio import AsyncEngine
-from sqlmodel import create_engine
+from sqlmodel import create_engine, text
 
 from src.config import Config
 
 engine = AsyncEngine(create_engine(url=Config.DATABASE_URL, echo=True))
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        statement = text("SELECT 'hello';")
+
+        result = await conn.execute(statement)
+        print(result.all())
